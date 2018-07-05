941e5b0ed3196a735d8e29029d90bf0c923222ea
nemosminer <slbaker@hotmail.co.nz>
2018-07-06 10:11:50 +1200

    Add files via upload

--- start-no-cfg-file.bat
+++ start-no-cfg-file.bat
@@ -21,20 +21,32 @@ REM     --pass           Worker password
 REM 
 REM  Options:
 REM     --help           Print this help
-REM     --list-devices   List available cuda devices to use
-REM     --dev            Space separated list of cuda devices
+REM     --list-devices   List available cuda devices
+REM 
+REM     --dev            Space separated list of cuda devices to use.
+REM                      If this option is not given all available devices
+REM                      are used.
+REM 
 REM     --time           Enable output of timestamps
 REM     --color          colorize the output
+REM 
 REM     --logfile        [=path] Append logs to the file named by 'path'
 REM                      If 'path' is not given append to 'zm.log' in
 REM                      current working directory.
 REM     --noreconnect    Disable automatic reconnection on network errors.
 REM 
-REM     --temp-target    In C - If set, enables temperature controller.
+REM     --temp-target    =dev_id:temp-target[,dev_id:temp-target] ...
+REM                      In C - If set, enables temperature controller.
 REM                      The workload of each GPU will be continuously
 REM                      adjusted such that the temperature stays around
 REM                      this value. It is recommended to set your fan speed
-REM                      to 100% when using this setting.
+REM                      to a constant value when using this setting.
+REM                      Example: --temp-target=0:65,2:70
+REM 
+REM     --intensity      =dev_id:intensity[,dev_id:intensity] ...
+REM                      Reduce the load which is put on the GPU - valid
+REM                      intensity range ]1.0-0.0[.
+REM                      Example: --intensity=0:0.893,2:0.8
 REM 
 REM     --telemetry      [=ip:port]. Starts telemetry server. Telemetry data
 REM                      can be accessed using a web browser(http) or by json-rpc.
@@ -46,6 +58,9 @@ REM     --cfg-file       [=path] Use configuration file. All additional command
 REM                      line options are ignored - configuration is done only
 REM                      through configuration file. If 'path' is not given
 REM                      use 'zm.cfg' in current working directory.
+REM 
+REM     --pool           =hostname,port_nr,user_name[,pass]
+REM                      Setup additional failover pools.
 REM 
 REM  Example:
 REM     zm --server servername.com --port 1234 --user username
